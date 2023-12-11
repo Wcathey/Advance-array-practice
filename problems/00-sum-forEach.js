b685f511851c5c2de61f60ed9d361133b22a643b
William <wcathey1604@gmail.com>
2023-12-10 23:35:03 -0600

    sum-forEach passed

--- problems/00-sum-forEach.js
+++ problems/00-sum-forEach.js
@@ -12,8 +12,15 @@ console.log(sumForEach([])); // 0
 */
 
 let sumForEach = function(arr) {
-  // Your code here 
-};
+  let sum = 0;
+
+  arr.forEach(number => {
+    sum += number;
+
+  });
+  return sum;
+}
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
